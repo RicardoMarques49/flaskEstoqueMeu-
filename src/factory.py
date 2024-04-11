62ad76452670f6e886c9cdd6293bdb001bcff29d
ALUNO <ricardoimep@gmail.com>
2024-04-10 21:58:03 -0300

    Aula 04_04 seeding banco

--- src/factory.py
+++ src/factory.py
@@ -1,11 +1,15 @@
 import json
+import logging
 import os
 import sys
+import uuid
 
 from flask import Flask, render_template
 
-from src.modulos import bootstrap,minify,db
+from src.modulos import bootstrap,minify,db, login
 from src.utils import existe_esquema, seeding
+from src.models import User
+
 
 
 def create_app(config_filename: str = 'config.dev.json') -> Flask:
@@ -29,6 +33,22 @@ def create_app(config_filename: str = 'config.dev.json') -> Flask:
     if app.config.get('MINIFY', False):
         minify.init_app(app)
     db.init_app(app)
+    login.init_app(app)
+    login.login_view = 'auth.login'
+    login.login_message ="Você precisa estar logado para acessar esse endereço"
+    login.login_message_category = 'warning'
+
+    @login.user_loader
+    def load_user(user_id):
+        try:
+            auth_id = uuid.UUID(str(user_id))
+        except ValueError:
+            return None
+        sentenca = db.select(User).where(User.id == auth_id)
+        usuario = db.session.execute(sentenca).scalar_one_none()
+        return usuario
+
+
 
     with app.app_context():
         if not existe_esquema(app):
@@ -42,5 +62,9 @@ def create_app(config_filename: str = 'config.dev.json') -> Flask:
     def index():
         return render_template('index.jinja',
                                title="Página principal")
+
+    from src.blueprints.auth import bp
+    app.register_blueprint(bp)
+
 
     return app
--- src/modulos.py
+++ src/modulos.py
@@ -1,7 +1,9 @@
 from flask_bootstrap import Bootstrap5
+from flask_login import LoginManager
 from flask_minify import Minify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from flask_login import LoginManager
 
 
 class Base(DeclarativeBase):
@@ -12,6 +14,8 @@ bootstrap = Bootstrap5()
 minify = Minify()
 db = SQLAlchemy(model_class=Base,
                 disable_autonaming=True)
+
+login = LoginManager()
 
 
 
